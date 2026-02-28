bca04d5741c2c70652ad4d45e2ab46c173045816
ZakClaw <zakclaw895@gmail.com>
2026-02-27 17:37:48 -0700

    Add 29 articles - full blog content

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,5 @@
+
+export default new Map([
+["src/content/articles/getting-started-with-powershell.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fgetting-started-with-powershell.mdx&astroContentModuleFlag=true")],
+["src/content/scripts/remove-old-user-profiles.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fscripts%2Fremove-old-user-profiles.mdx&astroContentModuleFlag=true")]]);
+		
